c252eeb954e6e0bff03d0d630d2cf8898e9a5eb6
Çağdaş Kurultay Kalkan <ckk@Cagdas-MacBook-Pro.local>
2020-09-13 18:37:48 +0300

    hotfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vakifbank",  # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Çağdaş Kurultay Kalkan",
     author_email="kurultay@codeck.com.tr",
     description="Vakıfbank Payment Gateway Python Package",
